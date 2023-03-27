78001eaa00e0d3818d0d5cd314c462fd6f8f4afa
Artem Musalitin <38328305+MrRTi@users.noreply.github.com>
2023-03-27 10:53:56 +0300

    Update ssh-keys.sh

--- scripts/ssh-keys.sh
+++ scripts/ssh-keys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Create ssh_keys
-ssh-keygen -t ed25519 -C 'gitlab-remote' -P '' -f ~/.ssh/gitlab
-ssh-keygen -t ed25519 -C 'github-remote' -P '' -f ~/.ssh/github
+[[ ! -f ~/.ssh/gitlab ]] ssh-keygen -t ed25519 -C 'gitlab-remote' -P '' -f ~/.ssh/gitlab
+[[ ! -f ~/.ssh/github ]] ssh-keygen -t ed25519 -C 'github-remote' -P '' -f ~/.ssh/github
 
 [[ ! -f ~/.ssh/config ]] && touch ~/.ssh/config
 echo "" >> ~/.ssh/config
