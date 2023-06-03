3c6fb992f50ea345c36c0071c117c64c94f3fa11
Artem Musalitin <a.musalitin@amusalitin.local>
2023-06-03 13:19:57 +0300

    Add kitty config

--- scripts/link-files.sh
+++ scripts/link-files.sh
@@ -2,9 +2,9 @@
 
 ln -sf $(pwd)/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf
 ln -sf $(pwd)/vim-config/.vimrc ~/.vimrc
-ln -sf $(pwd)/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+# ln -sf $(pwd)/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf $(pwd)/.config/lsd ~/.config/lsd
 # ln -sf $(pwd)/.config/starship.toml ~/.config/starship.toml
 ln -sf ~/astronvim-config ~/.config/astronvim/lua/user
 ln -sf $(pwd)/zsh-config/.zsh_plugins.txt ~/.zsh_plugins.txt
-
+ln -sf $(pwd)/.config/kitty ~/.config/kitty
