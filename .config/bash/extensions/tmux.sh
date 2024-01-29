b1f26775c727e34e58f2ac5a0d17526270adf082
Artem Musalitin <musalitin.a.a@gmail.com>
2024-01-29 12:31:21 +0100

    Update session flow funcs for tmux

--- .config/bash/extensions/tmux.sh
+++ .config/bash/extensions/tmux.sh
@@ -4,7 +4,7 @@
 # It prevents to find session for folder using folder name if folder have . as first symbol
 # We will use this conversion to have consistent session names on attach and create
 function replace_first_dot_with_underscore() {
-  echo $1 | sed "s/^\./_/g"
+  basename $1 | sed "s/^\./_/g"
 }
 
 # NOTE: Tmux attach on switch if already in tmux
@@ -19,9 +19,7 @@ function tmux_switch() {
 # NOTE: Tmux new session in specified or current folder
 function tmux_new_session() {
   SESSION_PATH=${1:-$(pwd)}
-  SESSION_NAME=$(replace_first_dot_with_underscore ${SESSION_PATH##*/})
-  echo $SESSION_PATH
-  echo $SESSION_NAME
+  SESSION_NAME=$(replace_first_dot_with_underscore ${SESSION_PATH})
   TMUX= tmux new -c ${SESSION_PATH} -s ${SESSION_NAME} -d 
   tmux_switch ${SESSION_NAME}
 }
@@ -34,8 +32,8 @@ function tmux_attach() {
 
 # NOTE: Attach OR create new session in specified or current folder
 function tmux_attach_or_create() {
-  PWD_FOR_SESSION_NAME=replace_first_dot_with_underscore ${PWD##*/}
-  tmux_switch ${1:-$PWD_FOR_SESSION_NAME} || tmux_new_session ${1:-$(pwd)}
+  PWD_FOR_SESSION_NAME=$(replace_first_dot_with_underscore $(pwd))
+  tmux_switch ${1:-$PWD_FOR_SESSION_NAME} 2>/dev/null || tmux_new_session ${1:-$(pwd)}
 }
 
 alias t='tmux'
