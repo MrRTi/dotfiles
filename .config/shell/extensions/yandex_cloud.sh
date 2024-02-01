f27992e58223e8db357424848cf253c05e008cd6
Artem Musalitin <musalitin.a.a@gmail.com>
2024-02-01 15:28:38 +0100

    Update yandex cloud config

--- .config/shell/extensions/yandex_cloud.sh
+++ .config/shell/extensions/yandex_cloud.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+add_file "$HOME/yandex-cloud/path.bash.inc"
+add_file "$HOME/yandex-cloud/completion.zsh.inc"
+
 # NOTE: show vms
 ycls() {
  yc compute instance list --format json | jq --raw-output '.[] | "\(.name) \(.status)"'
