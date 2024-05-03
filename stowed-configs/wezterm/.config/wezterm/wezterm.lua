fcff19d35ed101738458548e77c2f3dd3770b079
Artem Musalitin <musalitin.a.a@gmail.com>
2024-05-03 09:58:54 +0200

    lint wezterm config

--- stowed-configs/wezterm/.config/wezterm/wezterm.lua
+++ stowed-configs/wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -10,20 +10,21 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-function get_appearance()
+local function get_appearance()
   if wezterm.gui then
     return wezterm.gui.get_appearance()
   end
-  return 'Dark'
+  return "Dark"
 end
 
-function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'Catppuccin Mocha'
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "Catppuccin Mocha"
   else
-    return 'Catppuccin Latte'
+    return "Catppuccin Latte"
   end
 end
+
 config.color_scheme = scheme_for_appearance(get_appearance())
 
 config.enable_tab_bar = false
@@ -34,19 +35,19 @@ config.initial_cols = 80
 config.initial_rows = 24
 
 config.window_padding = {
-  left    = '20px',
-  right   = '20px',
-  top     = '20px',
-  bottom  = '10px',
+  left = "20px",
+  right = "20px",
+  top = "20px",
+  bottom = "10px",
 }
 
 config.window_background_opacity = 0.95
 
 config.font_size = 13.0
-config.font = wezterm.font {
-  family = 'JetBrains Mono',
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-}
+config.font = wezterm.font({
+  family = "JetBrains Mono",
+  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
 -- config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
 
 -- and finally, return the configuration to wezterm
