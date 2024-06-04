fa808db328b4506ba6d8a1e712b85532526c8d8f
Artem Musalitin <musalitin.a.a@gmail.com>
2024-06-04 16:08:38 +0200

    Update harpoon config

--- stowed-configs/nvim/.config/nvim/lua/plugins/harpoon.lua
+++ stowed-configs/nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -28,6 +28,25 @@ return {
 				harpoon:list():append()
 			end, { desc = "[h]arpoon [a]dd" })
 
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
 			local toggle_opts = {
 				border = "rounded",
 				title_pos = "center",
@@ -35,9 +54,13 @@ return {
 				ui_height_ratio = 0.40,
 			}
 
-			vim.keymap.set("n", "<leader>hq", function()
+			vim.keymap.set("n", "<leader>hm", function()
 				harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts)
-			end, { desc = "[h]arpoon [q]uick menu" })
+			end, { desc = "[h]arpoon quick [m]enu (original)" })
+
+			vim.keymap.set("n", "<leader>hq", function()
+				toggle_telescope(harpoon:list())
+			end, { desc = "[h]arpoon [q]uick menu (telescope)" })
 
 			vim.keymap.set("n", "<leader>h]", function()
 				harpoon:list():next()
