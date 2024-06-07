b6b79465b5c5cbbf713c4c3b37ec4d65e19315e9
Artem Musalitin <musalitin.a.a@gmail.com>
2024-06-07 23:10:09 +0200

    Add zen mode to nvim

--- stowed-configs/wezterm/.config/wezterm/wezterm.lua
+++ stowed-configs/wezterm/.config/wezterm/wezterm.lua
@@ -7,22 +7,22 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return "Dark"
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
 end
 
 local function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
-  end
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
 end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
@@ -31,20 +31,45 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
 config.window_padding = {
-  left = "20px",
-  right = "20px",
-  top = "20px",
-  bottom = "10px",
+	left = "20px",
+	right = "20px",
+	top = "20px",
+	bottom = "10px",
 }
 
 config.window_background_opacity = 0.95
 
 config.font_size = 13.0
 config.font = wezterm.font({
-  family = "JetBrains Mono",
-  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	family = "JetBrains Mono",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
 -- config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+
+-- nvim zen mode 
+-- https://github.com/folke/zen-mode.nvim#wezterm
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
 
 -- and finally, return the configuration to wezterm
 return config
