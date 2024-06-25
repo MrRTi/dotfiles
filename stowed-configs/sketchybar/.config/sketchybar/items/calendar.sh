5c39d16380c0131ca589a90a828ffc111e9abaf3
Artem Musalitin <musalitin.a.a@gmail.com>
2024-06-25 17:27:50 +0200

    Remove sketchybar config

--- stowed-configs/sketchybar/.config/sketchybar/items/calendar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sketchybar --add item calendar right \
-                --set calendar icon=􀧞 \
-                update_freq=3- \
-                script="$PLUGIN_DIR/calendar.sh" \
-                padding_right=-6
-
