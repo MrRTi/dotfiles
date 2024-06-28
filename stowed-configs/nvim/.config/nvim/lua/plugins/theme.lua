3cf9935fa58fd0b004b4df0d86b2c5e4e12ca0ab
Artem Musalitin <musalitin.a.a@gmail.com>
2024-06-28 09:42:45 +0200

    Change telescope bg to transparent

--- stowed-configs/nvim/.config/nvim/lua/plugins/theme.lua
+++ stowed-configs/nvim/.config/nvim/lua/plugins/theme.lua
@@ -44,6 +44,7 @@ return {
 
 					vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 					vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+					vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 				end,
 			})
 		end,
