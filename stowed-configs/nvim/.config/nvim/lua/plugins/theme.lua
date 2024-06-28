45c3293e136caace884da80c08cb63947e08ef30
Artem Musalitin <musalitin.a.a@gmail.com>
2024-06-28 09:29:04 +0200

    Change theme and lualine config

--- stowed-configs/nvim/.config/nvim/lua/plugins/theme.lua
+++ stowed-configs/nvim/.config/nvim/lua/plugins/theme.lua
@@ -72,15 +72,97 @@ return {
 						require("newpaper").setup({
 							style = "light",
 						})
+						require("lualine").setup({
+							options = {
+								theme = "newpaper-light",
+							},
+						})
 					else
 						require("newpaper").setup({
 							style = "dark",
+						})
+						require("lualine").setup({
+							options = {
+								theme = "newpaper-dark",
+							},
 						})
 					end
 
 					vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 					vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 				end,
+			})
+		end,
+	},
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					-- Theme changed by dark_notify
+					-- theme = "newpaper-dark",
+					theme = "newpaper-light",
+					-- empty with newpaper colorscheme
+					section_separators = { "", "" },
+					component_separators = { "│", "│" },
+				},
+				sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {
+						{
+							"filename",
+							newfile_status = false,
+							path = 1,
+							shorting_target = 80,
+							padding = {
+								left = 0,
+								right = 1,
+							},
+						},
+					},
+					lualine_x = {
+						"branch",
+						"diff",
+						"diagnostics",
+						"location",
+						{
+							"progress",
+							padding = {
+								left = 1,
+								right = 0,
+							},
+						},
+					},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {
+						{
+							"filename",
+							path = 1,
+						},
+					},
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+				winbar = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
 			})
 		end,
 	},
