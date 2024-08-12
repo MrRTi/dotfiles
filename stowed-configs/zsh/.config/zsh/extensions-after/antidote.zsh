782414f58f3ef78c2f0a9ef96b2afc4bfb65cd0f
Artem Musalitin <musalitin.a.a@gmail.com>
2024-08-12 09:41:12 +0200

    Fix error in antidote setup

--- stowed-configs/zsh/.config/zsh/extensions-after/antidote.zsh
+++ stowed-configs/zsh/.config/zsh/extensions-after/antidote.zsh
@@ -7,7 +7,7 @@ source_antidote () {
     source ${ZDOTDIR:-~}/.antidote/antidote.zsh
   else
     git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
-    source ${ZDOTDIR:-~}/.antidote/antidote.zshw
+    source ${ZDOTDIR:-~}/.antidote/antidote.zsh
   fi
 }
 
