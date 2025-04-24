1a23b37e7bdcdd37203f34dd6f214da22d9775ae
Artem Musalitin <artem@musalitin.me>
2025-04-24 15:24:45 +0200

    Add git config setup script

---
+++ git.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# NOTE: input format "folder1:email1;folder2:email2"
+#
+parts=$(echo "$1" | tr ";" "\n")
+
+for line in $parts; do
+	folder="$(cut -d ':' -f1 <<<$line)"
+	email="$(cut -d ':' -f2 <<<$line)"
+
+	path="$HOME/Developer/$folder"
+	gitconfig_path="$path/.gitconfig"
+
+	mkdir -pv "$path"
+	touch "$gitconfig_path"
+
+	echo -e "[includeIf \"gitdir:$path/**\"]\n  path = $gitconfig_path" >>"$HOME/.config/git/if-config"
+	git config --file $gitconfig_path user.email "$email"
+	key=$(ssh-add -L | grep "$email" | head -n 1)
+	git config --file $gitconfig_path user.signingkey "$key"
+
+	echo "Set git config values for repos at $path"
+	echo -e "\t- email=$email"
+	echo -e "\t- signingkey=$key"
+done
