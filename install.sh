8811be004a3e9a8988846340a9a8229f69e68491
Artem Musalitin <artem@musalitin.me>
2025-04-24 09:20:44 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -3,8 +3,43 @@
 ./brew.sh -i
 ./brew.sh -r
 
+# NOTE: Use brew in next commands
+# /opt/homebrew for macOS on Apple Silicon,
+# /usr/local for macOS on Intel, and
+# /home/linuxbrew/.linuxbrew for Linux.
+#
+if [ -f "/opt/homebrew/bin/brew" ]; then
+	BREW_PATH="/opt/homebrew"
+elif [ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
+	BREW_PATH="/home/linuxbrew/.linuxbrew"
+else
+	BREW_PATH="/use/local"
+fi
+
+eval "$($BREW_PATH/bin/brew)"
+
+mkdir -p "$HOME/.1password"
+
+# NOTE: Link macOS specific paths
+if [[ "$(uname)" == "Darwin" ]]; then
+	ln -s "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" "$HOME/.1password/agent.sock"
+	ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs" "$HOME/iCloud"
+fi
+
+export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
+
+sudo echo $(which fish) >>/etc/shells
+chsh -s $(which fish)
+
 ./link.sh -a -y
 
+DEV_FOLDER="$HOME/Developer/personal"
+
 if [ ! -d "$HOME/.config/nvim" ]; then
-	git clone -b lazyvim https://github.com/MrRTi/nvim-config.git $HOME/.config/nvim
+	git clone -b lazyvim https://github.com:MrRTi/nvim-config.git "$DEV_FOLDER/nvim-config"
+	ln -s "$DEV_FOLDER/nvim-config" "$HOME/.config/nvim"
+
+	cd "$DEV_FOLDER/nvim-config"
+	git remote set-url origin git@github.com:MrRTi/nvim-config.git
+	cd "$HOME"
 fi
