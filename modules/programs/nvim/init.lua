e2cae73534c04641a84a57a4ed2b1893a54fbb26
Artem Musalitin <artem@musalitin.me>
2025-07-29 15:44:24 +0400

    Change nvim keymaps

--- modules/programs/nvim/init.lua
+++ modules/programs/nvim/init.lua
@@ -83,9 +83,9 @@ vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>")
 
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-vim.keymap.set('n', '<space>lr', vim.lsp.buf.rename, opts)
+vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, opts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
