583cf12f8d9fac9df21356a0b5894d2b05bede5b
Artem Musalitin <artem@musalitin.me>
2025-08-06 15:46:29 +0400

    Fix vim global in nvim/init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -62,8 +62,8 @@ vim.lsp.enable({
 vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
-      diagnostics = {
-        globals = { "vim" }
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
       }
     }
   }
