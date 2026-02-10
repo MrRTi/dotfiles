caaf3ac35638e7fc1f024692c369fa970d24c96f
Artem Musalitin <artem@musalitin.me>
2026-02-10 21:05:46 +0100

    Refactoring

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -15,32 +15,36 @@ vim.opt.sidescrolloff = 999
 
 vim.opt.cursorline = true
 
-local indent_settings = {
-  python     = { tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true },
-  go         = { tabstop = 8, shiftwidth = 8, softtabstop = 0, expandtab = false }, -- tabs, no expand
-  make       = { tabstop = 8, shiftwidth = 8, softtabstop = 0, expandtab = false }, -- tabs, no expand
+-- NOTE: Default indent: 2 spaces (covers ruby, lua, yaml, json, javascript, shell)
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+
+local indent_overrides = {
+  python = { tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true },
+  go = { tabstop = 8, shiftwidth = 8, softtabstop = 0, expandtab = false },
+  make = { tabstop = 8, shiftwidth = 8, softtabstop = 0, expandtab = false },
   dockerfile = { tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true },
-  ["*"]      = { tabstop = 2, shiftwidth = 2, softtabstop = 2, expandtab = true },  -- default: 2 spaces
 }
 
-for filetype, opts in pairs(indent_settings) do
+for filetype, opts in pairs(indent_overrides) do
   vim.api.nvim_create_autocmd("FileType", {
     pattern = filetype,
     callback = function()
-      vim.bo.tabstop     = opts.tabstop
-      vim.bo.shiftwidth  = opts.shiftwidth
+      vim.bo.tabstop = opts.tabstop
+      vim.bo.shiftwidth = opts.shiftwidth
       vim.bo.softtabstop = opts.softtabstop
-      vim.bo.expandtab   = opts.expandtab
+      vim.bo.expandtab = opts.expandtab
     end,
   })
 end
 
--- Add ability to use йцукен letters same as qwerty. (symbols like :, $ etc won't work as expected)
-vim.o.langmap =
-    "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;" ..
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ," ..
-    "фисвуапршолдьтщзйкыегмцчня;" ..
-    "abcdefghijklmnopqrstuvwxyz,"
+-- NOTE: Add ability to use йцукен letters same as qwerty. (symbols like :, $ etc won't work as expected)
+vim.o.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;"
+    .. "ABCDEFGHIJKLMNOPQRSTUVWXYZ,"
+    .. "фисвуапршолдьтщзйкыегмцчня;"
+    .. "abcdefghijklmnopqrstuvwxyz,"
 
 vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },
@@ -51,22 +55,19 @@ vim.pack.add({
   { src = "https://github.com/echasnovski/mini.completion" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/christoomey/vim-tmux-navigator" },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
-  { src = "https://github.com/kdheepak/lazygit.nvim" },
-  { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/folke/todo-comments.nvim" },
   {
     src = "https://github.com/ThePrimeagen/harpoon",
-    version = "harpoon2"
+    version = "harpoon2",
   },
   { src = "https://github.com/nvimtools/none-ls.nvim" },
   { src = "https://github.com/nvimtools/none-ls-extras.nvim" },
   { src = "https://github.com/gbprod/none-ls-shellcheck.nvim" },
+  { src = "https://github.com/folke/todo-comments.nvim" },
   { src = "https://github.com/nvim-neotest/neotest" },
   -- deps
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/nvim-neotest/nvim-nio" },
-  { src = "https://github.com/antoinemadec/FixCursorHold.nvim" },
   { src = "https://github.com/olimorris/neotest-rspec" },
   --
   { src = "https://github.com/andythigpen/nvim-coverage" },
@@ -75,16 +76,16 @@ vim.pack.add({
 })
 
 require("todo-comments").setup()
-require('mini.ai').setup()
-require('mini.completion').setup()
-require('mini.extra').setup()
-require('mini.splitjoin').setup()
+require("mini.ai").setup()
+require("mini.completion").setup()
+require("mini.extra").setup()
+require("mini.splitjoin").setup()
 require("coverage").setup({
   auto_reload = true,
 })
 require("neotest").setup({
   adapters = {
-    require("neotest-rspec")
+    require("neotest-rspec"),
   },
 })
 
@@ -102,12 +103,12 @@ vim.lsp.config("lua_ls", {
     Lua = {
       workspace = {
         library = vim.api.nvim_get_runtime_file("", true),
-      }
-    }
-  }
+      },
+    },
+  },
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     if client and client.server_capabilities.completionProvider then
@@ -117,7 +118,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.opt.completeopt:append("noselect")
-
 
 local null_ls = require("null-ls")
 local augroup_format = vim.api.nvim_create_augroup("NullLsFormat", { clear = true })
@@ -155,7 +155,7 @@ null_ls.setup({
   end,
 })
 
-require('nvim-treesitter.configs').setup({
+require("nvim-treesitter.configs").setup({
   ensure_installed = {
     "lua",
     "ruby",
@@ -164,67 +164,90 @@ require('nvim-treesitter.configs').setup({
     "yaml",
     "json",
   },
-  highlight = { enable = true }
+  highlight = { enable = true },
 })
 
-require('oil').setup({
+require("oil").setup({
   view_options = {
     show_hidden = true,
-  }
+  },
 })
 
-require('fzf-lua').setup({
+require("fzf-lua").setup({
   winopts = {
     preview = {
       layout = "vertical",
-    }
+    },
   },
   keymap = {
     fzf = {
       true,
-      -- Use <c-q> to select all items and add them to the quickfix list
+      -- NOTE: Use <c-q> to select all items and add them to the quickfix list
       ["ctrl-q"] = "select-all+accept",
     },
   },
 })
 
+-- NOTE: Tmux-aware pane navigation (replaces vim-tmux-navigator plugin)
+local function tmux_navigate(direction)
+  local tmux_dir = ({ h = "L", j = "D", k = "U", l = "R" })[direction]
+  local win = vim.api.nvim_get_current_win()
+  vim.cmd("wincmd " .. direction)
+  if vim.api.nvim_get_current_win() == win and vim.env.TMUX then
+    vim.fn.system("tmux select-pane -" .. tmux_dir)
+  end
+end
 
-vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = "Delete buffer" })
+for _, dir in ipairs({ "h", "j", "k", "l" }) do
+  vim.keymap.set("n", "<C-" .. dir .. ">", function()
+    tmux_navigate(dir)
+  end, { desc = "Navigate " .. dir })
+end
 
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y', { desc = "Yank to system clipboard" })
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d', { desc = "Delete to system clipboard" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
 
-vim.keymap.set('n', '<leader><space>', '<cmd>FzfLua global<CR>', { desc = "Search files and buffers" })
-vim.keymap.set('n', '<leader>sf', '<cmd>FzfLua files<CR>', { desc = "Search files" })
-vim.keymap.set('n', '<leader>sw', '<cmd>FzfLua grep_cword<CR>', { desc = "Search word under cursor" })
-vim.keymap.set('n', '<leader>sg', '<cmd>FzfLua live_grep<CR>', { desc = "Live grep" })
-vim.keymap.set('n', '<leader>sh', '<cmd>FzfLua helptags<CR>', { desc = "Search help tags" })
-vim.keymap.set('n', '<leader>sr', '<cmd>FzfLua resume<CR>', { desc = "Resume last search" })
+vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set({ "n", "v", "x" }, "<leader>d", '"+d', { desc = "Delete to system clipboard" })
 
-vim.keymap.set('n', '<leader>e', '<cmd>Oil<CR>', { desc = "Open file explorer" })
-vim.keymap.set('n', '<leader>-', '<cmd>Oil<CR>', { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader><space>", "<cmd>FzfLua global<CR>", { desc = "Search files and buffers" })
+vim.keymap.set("n", "<leader>sf", "<cmd>FzfLua files<CR>", { desc = "Search files" })
+vim.keymap.set("n", "<leader>sw", "<cmd>FzfLua grep_cword<CR>", { desc = "Search word under cursor" })
+vim.keymap.set("n", "<leader>sg", "<cmd>FzfLua live_grep<CR>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>sh", "<cmd>FzfLua helptags<CR>", { desc = "Search help tags" })
+vim.keymap.set("n", "<leader>sr", "<cmd>FzfLua resume<CR>", { desc = "Resume last search" })
 
-vim.keymap.set('n', '<leader>fp', '<cmd>let @+ = expand("%")<CR>', { desc = "Copy file path to clipboard" })
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>-", "<cmd>Oil<CR>", { desc = "Open file explorer" })
 
-vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>', { desc = "Open LazyGit" })
-vim.keymap.set({ 'n', 'v' }, '<leader>gb', '<cmd>Gitsigns blame_line<CR>', { desc = "Git blame line" })
-vim.keymap.set({ 'n', 'v' }, '<leader>gp', '<cmd>Gitsigns preview_hunk_inline<CR>', { desc = "Preview hunk inline" })
-vim.keymap.set('n', '[h', '<cmd>Gitsigns prev_hunk<CR>', { desc = "Previous git hunk" })
-vim.keymap.set('n', ']h', '<cmd>Gitsigns next_hunk<CR>', { desc = "Next git hunk" })
+vim.keymap.set("n", "<leader>fp", '<cmd>let @+ = expand("%")<CR>', { desc = "Copy file path to clipboard" })
 
-vim.keymap.set({ 'n', 'v' }, '<leader>lf', function()
+vim.keymap.set("n", "<leader>gg", function()
+  vim.cmd("tabnew | terminal lazygit")
+  vim.cmd("startinsert")
+  vim.api.nvim_create_autocmd("TermClose", {
+    buffer = 0,
+    callback = function()
+      vim.cmd("bdelete!")
+    end,
+  })
+end, { desc = "Open LazyGit" })
+vim.keymap.set({ "n", "v" }, "<leader>gb", "<cmd>Gitsigns blame_line<CR>", { desc = "Git blame line" })
+vim.keymap.set({ "n", "v" }, "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk inline" })
+vim.keymap.set("n", "[h", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Previous git hunk" })
+vim.keymap.set("n", "]h", "<cmd>Gitsigns next_hunk<CR>", { desc = "Next git hunk" })
+
+vim.keymap.set({ "n", "v" }, "<leader>lf", function()
   vim.lsp.buf.format({ async = true })
 end, { desc = "Format file or selection" })
 
-vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = "LSP rename" })
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "LSP rename" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
 
-vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float, { desc = "Open floating diagnostic window" })
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-
+vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, { desc = "Open floating diagnostic window" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
 
 local harpoon = require("harpoon")
 harpoon.setup()
@@ -245,24 +268,25 @@ vim.keymap.set("n", "]H", function()
   harpoon:list():next()
 end, { desc = "Next harpoon file" })
 
-
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank {
+    vim.highlight.on_yank({
       higroup = "IncSearch",
       timeout = 200,
-    }
+    })
   end,
 })
 
--- Function to toggle background color
+-- NOTE: Function to toggle background color
 vim.o.background = "dark"
 
 local function is_dark_local()
-  local handle = io.popen(
-    [[osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode']])
-  if not handle then return false end
+  local handle =
+      io.popen([[osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode']])
+  if not handle then
+    return false
+  end
   local result = handle:read("*a")
   handle:close()
   result = result:lower():gsub("%s+", "")
@@ -270,7 +294,7 @@ local function is_dark_local()
 end
 
 require("catppuccin").setup({
-  transparent_background = true
+  transparent_background = true,
 })
 
 vim.cmd("colorscheme catppuccin")
@@ -290,6 +314,6 @@ else
   toggle_appearance("light")
 end
 
-vim.keymap.set('n', '<leader>tt', toggle_appearance, { desc = "Toggle light/dark appearance" })
+vim.keymap.set("n", "<leader>tt", toggle_appearance, { desc = "Toggle light/dark appearance" })
 
 vim.cmd("hi statusline guibg=NONE")
