0732b2693e58de4857562a656c34efb04bd710f0
Artem Musalitin <artem@musalitin.me>
2026-05-21 12:25:18 +0200

    Use worktree folder name as tmux window name when switching

--- dot_config/fish/functions/git_worktree_switch.fish
+++ dot_config/fish/functions/git_worktree_switch.fish
@@ -1,5 +1,10 @@
 function git_worktree_switch
     set -l path (git_worktree_select $argv[1])
     test -z "$path"; and return 1
+
     cd $path
+
+    if test -n "$TMUX"
+        tmux rename-window (basename $path)
+    end
 end
