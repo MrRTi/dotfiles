2e104a8d61d73f40cb85a9958596dfc54abef535
Artem Musalitin <artem@musalitin.me>
2026-05-21 14:13:00 +0200

    Use worktree folder name as tmux window name when adding new worktree

--- dot_config/fish/functions/git_worktree_add_query.fish
+++ dot_config/fish/functions/git_worktree_add_query.fish
@@ -2,4 +2,8 @@ function git_worktree_add_query
     set -l path (command git_worktree_add_query $argv[1])
     test -z "$path"; and return 0
     cd $path
+
+    if test -n "$TMUX"
+        tmux rename-window (basename $path)
+    end
 end
